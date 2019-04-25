db96ec4a6cb75710344af12569ecbb96cb4fa9f2
RodrigoCruz1154 <00115417>
2019-04-25 10:26:03 -0600

    Endgame spoilers ahead

--- app/src/main/java/com/naldana/ejemplo10/RetroCrypto.kt
+++
@@ -1,9 +0,0 @@
-package com.naldana.ejemplo10
-
-import java.util.*
-
-class RetroCrypto {
-
-    data class RetroCrypto (val Country : String, val IsAvailable: String, val Name: String, val Value:String, val Value_US:String)
-
-}
